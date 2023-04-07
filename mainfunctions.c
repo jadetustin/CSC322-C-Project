4f3c0440f7fa5f9eb1eef2a9a77331a446b5a327
jadetustin <jadetustin@protonmail.com>
2023-04-07 19:06:53 -0400

    added mainfunctions.c this time
    cdkm
    sdl;mcsd
    :w
    w/sqwmcklenvlked
    dfklvldfc
    c
    c
    c
    c
    c
    c
    dsvmf

---
+++ mainfunctions.c
@@ -0,0 +1,8 @@
+//*************************************************************************************************
+#include <tdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "mainfunctions.h"
+#include "datatypes.h"
+#include "datafunctions.h"
+#include "constants.h"
