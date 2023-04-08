beb8992a91f56da5faf0686788988c3f11e18038
jadetustin <jadetustin@protonmail.com>
2023-04-07 20:08:38 -0400

    lots of untested code oh god oh fuck pt.2

---
+++ mainfunctions.h
@@ -0,0 +1,18 @@
+//*************************************************************************************************
+#ifndef MAINFUNCTIONS_H
+#define MAINFUNCTIONS_H
+
+#include "datatypes.h"
+#include "constants.h"
+#include "datafunctions.h"
+//*************************************************************************************************
+BoatDataType* add_boat(String userinput);
+void remove_boat(BoatDataType* boat);
+void add_to_database(DatabaseType database, int* size);
+void remove_from_database(DatabaseType database, int* size);
+void print_database(DatabaseType database, int size);
+void new_month(DatabaseType database, int size);
+void payment(DatabaseType database, int size);
+//*************************************************************************************************
+#endif
+//*************************************************************************************************
